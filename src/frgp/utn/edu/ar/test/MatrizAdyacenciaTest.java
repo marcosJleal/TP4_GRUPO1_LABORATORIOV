6db057e38e5175afcf372d2f95d4105cb70a0885
Mateo De Benedictis <mateodebenedictismazza@gmail.com>
2021-05-08 02:29:07 -0300

    Falta solo un metodo ahora, ya agregue dos mas
    Agregue los dos metodos g y h del tp, y arregle dos metodos que estaban mal.
    Hay uno (existenTodosLosElementoTest) que hay que chequearlo

--- src/frgp/utn/edu/ar/test/MatrizAdyacenciaTest.java
+++ src/frgp/utn/edu/ar/test/MatrizAdyacenciaTest.java
@@ -9,6 +9,7 @@ import frgp.utn.edu.ar.main.MatrizAdyacencia;
 class MatrizAdyacenciaTest {
 
 	private MatrizAdyacencia mat= new MatrizAdyacencia(2);
+	
 	@Test
 	public void agregarElementoTest() {
 		int X=1;
@@ -16,14 +17,16 @@ class MatrizAdyacenciaTest {
 		mat.agregarElemento(X, Y);
 		assertTrue(mat.existeElemento(X, Y),"Existe dentro de la matriz");
 	}
+	
 	@Test
 	public void agregarElementoSimetriaTest() {
 		int X=0;
 		int Y=1;
 		mat.agregarElemento(X, Y);
-		mat.agregarElemento(Y, X);
+		//mat.agregarElemento(Y, X); Esta linea no va, pide que se verifique si se agrego tambien en su posicion opuesta, no hay que volver a agregarlo
 		assertEquals(true,mat.existeElemento(Y, X));
 	}
+	
 	@Test
 	public void eliminarElementoTest() {
 		int X=0;
@@ -31,19 +34,26 @@ class MatrizAdyacenciaTest {
 		mat.eliminarElemento(X, Y);
 		assertFalse(mat.existeElemento(X, Y));
 	}
+	
 	@Test
 	public void eliminarElementoSimetricoTest() {
 		int X=0;
 		int Y=1;
 		mat.eliminarElemento(X, Y);
 		assertFalse(mat.existeElemento(Y, X));
-	
 	}
+	
 	@Test
 	public void contarRelacionesTest() {
-			
-		assertEquals(mat.getCantidadElementos(),2);
+		//Este test estaba mal porque no se le habia agregado ningun elemento a la matriz
+		int X=0;
+		int Y=1;
+		mat.agregarElemento(X, Y);
+		//Despues de agregar un elemento, se pregunta cuantos hay, y se compara con 1 (Elemento)
+		assertEquals(mat.getCantidadElementos(),1);
 	}
+	
+	//Hay que revisar esta funcion, solo se le agrega un elemento, y solo se chequea ese elemento
 	@Test
 	public void  existenTodosLosElementoTest() {
 		int X=0;
@@ -53,12 +63,49 @@ class MatrizAdyacenciaTest {
 		cantidad=mat.getCantidadElementos();
 		for( int i=0; i<cantidad-1; i++)
 		{
-			
-			   for(int j=0; j<=cantidad-1; j++)
-			   {
-				   assertFalse(mat.existeElemento(Y, X));
+			for(int j=0; j<=cantidad-1; j++)
+			{
+				assertFalse(mat.existeElemento(Y, X)); 
 			}
 		}
 	}
+	
+	@Test
+	public void agregarElementoFilaNegativaTest() {
+		int X=-1;
+		int Y=1;
+		boolean error = false;
+		
+		try {
+			mat.agregarElemento(X, Y);
+		} catch(ArrayIndexOutOfBoundsException e) {
+			e.printStackTrace();
+			error = true;
+		}
+		assertTrue(error);
+	}
+
+	
+	@Test
+	public void agregarElementoColumnaNegativaTest() {
+		int X=1;
+		int Y=-2;
+		boolean error = false;
+		
+		try {
+			mat.agregarElemento(X, Y);
+		} catch(ArrayIndexOutOfBoundsException e) {
+			e.printStackTrace();
+			error = true;
+		}
+		assertTrue(error);
+	}
+	
+	/*
+	@Test
+	public void agregarElementoFueraRangoTest() {
+		
+	}
+	*/
 	
 }
